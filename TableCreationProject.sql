588ce94f58d64c51cbd9e5d6b0f77af2d6f600b9
bmkinney6 <126104497+bmkinney6@users.noreply.github.com>
2024-11-11 18:40:56 -0600

    Updated table creation file
    added on to the table creation file, in working condition

--- TableCreationProject.sql
+++ TableCreationProject.sql
@@ -1,70 +1,110 @@
-Create schema Project;
-use Project;
+-- Create the Project schema
+CREATE SCHEMA Project;
+USE Project;
 
-Create table Servers
-(
-	ServerID int primary key auto_increment,
-    HostGame varchar(45) , 
-    Capacity int ,
-    PricePerDay int 
+-- Table: GameArea
+CREATE TABLE GameArea (
+    AreaID VARCHAR(45) PRIMARY KEY,  -- Primary key for GameArea
+    AreaName VARCHAR(45),  -- Name of the area in the casino
+    CurrencyType VARCHAR(45)  -- Type of currency taken in the game area
 );
 
-Create table Customers
-(
-	SSN varchar(45) primary key,
-    FirstName varchar(45) ,
-    MiddleName varchar(45) , 
-    LastName varchar(45) ,
-    Address varchar(45) ,
-    State varchar(2) ,
-    City varchar(45) ,
-    BirthDate varchar(45) 
-    #Banned ? 
+-- Table: Cards
+CREATE TABLE Cards (
+    CardID INT PRIMARY KEY AUTO_INCREMENT,  -- Unique ID of the casino card
+    CreditCard_FirstName VARCHAR(45),  -- First name on the credit card
+    CreditCard_LastName VARCHAR(45),  -- Last name on the credit card
+    CreditCard_Number VARCHAR(16),  -- Credit card number
+    CreditCard_CVS CHAR(3)  -- Credit card security code
 );
 
-Create table Games
-(
-	GameID int primary key ,
-    GameName varchar(45) ,
-    HostedServer int ,
-    GameDate Date ,
-    Player varchar(45) ,
-    Bet int , 
-    Cashout int ,
-    foreign key (Player) references Customers(SSN) ,
-    foreign key (HostedServer) references Servers(ServerID) 
+-- Table: Wallet
+CREATE TABLE Wallet (
+    WalletID INT PRIMARY KEY AUTO_INCREMENT,  -- Unique ID for wallet
+    ChipValue DECIMAL(10,2),  -- Amount of money the person has in casino credit (counted in USD)
+    CardID INT,  -- Users can have a casino card that stores credit card info.
+    FOREIGN KEY (CardID) REFERENCES Cards(CardID)  -- Foreign key for CardID
 );
 
-#maybe add employees table so each ticket has an employee ID
-Create table Support_Tickets
-(
-	TicketID int primary key auto_increment ,
-    Customer varchar(45) ,
-    Subject varchar(45) ,
-    Text varchar(45) ,
-    
-    foreign key (Customer) references Customers(SSN) 
+-- Table: Employees
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY AUTO_INCREMENT,  -- Changed to INT and auto-increment for primary key
+    SSN CHAR(9) UNIQUE,  -- SSN of the employee
+    FirstName VARCHAR(45),  -- First name of the employee
+    MiddleName VARCHAR(45),  -- Middle name of the employee
+    LastName VARCHAR(45),  -- Last name of the employee
+    StreetNumber VARCHAR(45),  -- Street number of the employee
+    StreetName VARCHAR(45),  -- Street name of the employee
+    City VARCHAR(45),  -- City of the employee
+    State CHAR(2),  -- State of the employee
+    Postal CHAR(5),  -- Postal code of the employee
+    BirthDate DATE,  -- Birth date of the employee
+    AreaID VARCHAR(45),  -- Area the employee works in, references GameArea.AreaID
+    FOREIGN KEY (AreaID) REFERENCES GameArea(AreaID)
+);
+
+-- Table: Games
+CREATE TABLE Games (
+    GameID INT PRIMARY KEY AUTO_INCREMENT,  -- Primary key, each game is assigned unique ID
+    GameName VARCHAR(45),  -- Name for the casino game
+    AreaID VARCHAR(45),  -- References GameArea.AreaID to determine which section of the casino the game is in
+    MinBet INT,  -- Records the minimum bet of the game
+    MaxBet INT,  -- Records the maximum bet of the game
+    MoneyIn DECIMAL(10,2),  -- Records total money in, references total Winnings in PlayedGames table
+    MoneyOut DECIMAL(10,2),  -- Records total money out, references total Losings in PlayedGames table
+    FOREIGN KEY (AreaID) REFERENCES GameArea(AreaID)
+);
+
+-- Table: Customers
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY AUTO_INCREMENT,  -- Primary key for customer
+    SSN CHAR(9) UNIQUE,  -- Social security number of the customer
+    FirstName VARCHAR(45),  -- First name of the customer
+    MiddleName VARCHAR(45),  -- Middle name of the customer
+    LastName VARCHAR(45),  -- Last name of the customer
+    PhoneNumber VARCHAR(15), -- Phone number of the customer
+    StreetNumber VARCHAR(45),  -- Street number of the customer
+    StreetName VARCHAR(45),  -- Street name of the customer
+    City VARCHAR(45),  -- City of the customer
+    State CHAR(2),  -- State of the customer
+    Postal CHAR(5),  -- Postal code of the customer
+    BirthDate DATE,  -- Birth date of the customer
+    WalletID INT,  -- References Wallet.WalletID
+    Banned BOOLEAN,  -- True if the customer is banned
+    FOREIGN KEY (WalletID) REFERENCES Wallet(WalletID)
+);
+
+-- Table: PlayedGames
+CREATE TABLE PlayedGames (
+    PlayedGameID INT PRIMARY KEY AUTO_INCREMENT,  -- Unique ID for each played game
+    GameID INT,  -- References Games.GameID to know which game was played
+    GameDate DATE,  -- Date the game was played
+    CustomerID INT,  -- References Customers.CustomerID
+    Bet INT,  -- Bet made by the customer
+    Winnings INT,  -- Amount won (could be null)
+    Losings INT,  -- Amount lost (could be null)
+    FOREIGN KEY (GameID) REFERENCES Games(GameID),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+-- Table: Support_Tickets
+CREATE TABLE Support_Tickets (
+    TicketID INT PRIMARY KEY AUTO_INCREMENT,  -- Unique ID of the support ticket
+    CustomerID INT,  -- References Customers.CustomerID
+    Subject VARCHAR(45),  -- Type of complaint (e.g., 5 types)
+    Text TEXT,  -- Text of the complaint
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+-- Table: BannedList
+CREATE TABLE BannedList (
+    CustomerID INT PRIMARY KEY,  -- References Customers.CustomerID
+    FirstName VARCHAR(45),  -- Banned customer's first name, references Customers.FirstName
+    MiddleName VARCHAR(45),  -- Banned customer's middle name, references Customers.MiddleName
+    LastName VARCHAR(45),  -- Banned customer's last name, references Customers.LastName
+    PhoneNumber VARCHAR(15),  -- Customer phone number (added as it's not in Customers table)
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
 
 
-#maybe make this into 2 for deposit and withdraws
-Create table Transaction
-(
-	TransactionID int primary key auto_increment ,
-    Deposit int ,
-    CreditCard varchar(45),
-    CreditCard_Owner varchar(45), 
-    CreditCard_CVS int(3) ,
-    Bank varchar(45) , 
-	Address varchar(45) ,
-    State varchar(2) ,
-    City varchar(45) ,
-    Customer varchar(45) ,
-    TransactionDate date ,
-    foreign key (Customer) references Customers(SSN) 
-);
-
-Create table BannedList
-(
-) 
 
