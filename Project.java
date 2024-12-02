b5edb32c0d9758de93b2b1b2cddef4da8114cd04
Alexandro Alonso <159957257+AlexAlonso002@users.noreply.github.com>
2024-12-01 19:55:26 -0600

    Update Project.java

--- Project.java
+++ Project.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
-public class Project {
+public class AgustinSQL {
 
     public static void main(String[] args) {
         // Database connection details
